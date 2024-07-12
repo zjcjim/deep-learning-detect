1a5b95347dd7ae0c8a127366456fc87844eb03ac
张锦晨 <zjcjim@outlook.com>
2024-07-12 15:39:53 +0800

    chore: Update object confidence threshold to 0.65

--- detect.py
+++ detect.py
@@ -95,6 +95,7 @@ def send_position():
                 print('发送请求时发生错误:', e)
         
         time.sleep(0.2)
+        data = 0
 
 
 def detect(save_img=False):
@@ -275,7 +276,7 @@ if __name__ == '__main__':
     parser.add_argument('--weights', nargs='+', type=str, default='yolov7.pt', help='model.pt path(s)')
     parser.add_argument('--source', type=str, default='inference/images', help='source')  # file/folder, 0 for webcam
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
-    parser.add_argument('--conf-thres', type=float, default=0.7, help='object confidence threshold')
+    parser.add_argument('--conf-thres', type=float, default=0.65, help='object confidence threshold')
     parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
     parser.add_argument('--device', default='0', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     parser.add_argument('--view-img', action='store_true', help='display results')
